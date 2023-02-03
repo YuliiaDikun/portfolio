1830399f66f42dcd0455f7986ce2bd05946ae884
Yuliia Dikun <dikun.yulia@gmail.com>
2023-02-03 13:33:37 +0200

    create: navbar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import NavBar from "./components/NavBar/NavBar";
+
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <NavBar />
+    </div>
+  );
 }
 
 export default App;
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,11 +1,70 @@
-import React from 'react'
+import React, { useState } from "react";
 
+import { FaBars, FaTimes } from "react-icons/fa";
 const NavBar = () => {
+  const [nav, setNav] = useState(false);
+  const links = [
+    {
+      id: 1,
+      link: "home",
+    },
+    {
+      id: 2,
+      link: "about",
+    },
+    {
+      id: 3,
+      link: "portfolio",
+    },
+    {
+      id: 4,
+      link: "experience",
+    },
+    {
+      id: 5,
+      link: "contact",
+    },
+  ];
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <div className="flex justify-between items-center w-full px-4 h-20 text-white bg-black fixed">
+      <div>
+        <h1 className="text-5xl font-signature ml-2">Yuliia</h1>
+      </div>
+      <ul className="hidden md:flex">
+        {links.map(({ id, link }) => {
+          return (
+            <li
+              key={id}
+              className="px-4 cursor-pointer capitalize font-medium text-gray-500 hover:scale-105 duration-200"
+            >
+              {link}
+            </li>
+          );
+        })}
+      </ul>
+      <div
+        onClick={() => setNav(!nav)}
+        className="cursor-pointer pr-4 z-10 text-gray-500 md:hidden"
+      >
+        {nav ? <FaTimes size={30} /> : <FaBars size={30} />}
+      </div>
 
-export default NavBar
+      {nav && (
+        <ul className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-gray-900 text-gray-500">
+          {links.map(({ id, link }) => {
+            return (
+              <li
+                key={id}
+                className="px-4 cursor-pointer capitalize py-6 text-4xl"
+              >
+                {link}
+              </li>
+            );
+          })}
+        </ul>
+      )}
+    </div>
+  );
+};
+
+export default NavBar;
