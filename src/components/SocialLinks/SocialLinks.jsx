8e3438292fd3f43dfe55a3db1d87c46f96563272
Yuliia Dikun <dikun.yulia@gmail.com>
2023-02-03 14:46:49 +0200

    fix: social links is done

--- src/components/SocialLinks/SocialLinks.jsx
+++ src/components/SocialLinks/SocialLinks.jsx
@@ -1,9 +1,78 @@
 import React from "react";
 import { FaGithub, FaLinkedin } from "react-icons/fa";
-import { HiOutLineMail } from "react-icons/hi";
+import { HiOutlineMail } from "react-icons/hi";
 import { BsFillPersonLinesFill } from "react-icons/bs";
 const SocialLinks = () => {
-  return <div></div>;
+  const links = [
+    {
+      id: 1,
+      child: (
+        <>
+          LinkedIn <FaLinkedin size={30} />
+        </>
+      ),
+      href: "https://www.linkedin.com/in/yuliia-dikun/",
+      style: "rounded-tr-md",
+    },
+    {
+      id: 2,
+      child: (
+        <>
+          GitHub <FaGithub size={30} />
+        </>
+      ),
+      href: "https://github.com/YuliiaDikun",
+    },
+    {
+      id: 3,
+      child: (
+        <>
+          Mail <HiOutlineMail size={30} />
+        </>
+      ),
+      href: "mailto:jul.dikun@gmail.com",
+    },
+    {
+      id: 4,
+      child: (
+        <>
+          Resume <BsFillPersonLinesFill size={30} />
+        </>
+      ),
+      href: "/Yullia_Dikun_Frontend_Developer.pdf",
+      style: "rounded-br-md",
+      download: true,
+    },
+  ];
+
+  return (
+    <div className="hidden lg:flex flex-col top-[35%] left-0 fixed">
+      <ul>
+        {links.map(({ id, child, href, style, download }) => {
+          return (
+            <li
+              key={id}
+              className={
+                "flex justify-between items-center w-40 h-14 px-4 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300 bg-gray-500" +
+                " " +
+                style
+              }
+            >
+              <a
+                href={href}
+                download={download}
+                target='_blank'
+                rel="noreferrer"
+                className="flex justify-between items-center w-full text-white"
+              >
+                {child}
+              </a>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
 };
 
 export default SocialLinks;
