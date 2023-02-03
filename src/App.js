a1426d968c50842f36b1e64ae7d6b7c62ba8cec2
Yuliia Dikun <dikun.yulia@gmail.com>
2023-02-03 13:03:17 +0200

    feat:fonts

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 function App() {
-  return <dib>hello</dib>;
+  return <div></div>;
 }
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
+    fontFamily: {
+      signature: ["Great Vibes"],
+    },
   },
   plugins: [],
 };
